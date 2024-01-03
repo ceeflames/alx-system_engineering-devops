25f46038f2ea5fedb3cf22f3ae3e804a07aee2c3
ceeflames <ceeflames@yahoo.com>
2024-01-02 23:41:04 -0800

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+    Using REST API, returns information about TODO list progress
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit(1)
+
+    url = "https://jsonplaceholder.typicode.com/"
+    users = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todo = requests.get(url + "todo", params={"userId": sys.argv[1]}).json()
+
+    completed_task = [i.get(
+        "title") for i in todo if i.get(
+            "completed_task") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        users.get("name"), len(completed_task), len(todo)))
+    [print("i {}".format(c)) for c in completed_task]
