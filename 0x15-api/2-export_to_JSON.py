b18aa3cd6900cad1562127cae6a90da752457bc4
ceeflames <ceeflames@yahoo.com>
2024-01-03 22:49:15 -0800

    export to JSON

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+    Extending the python script to export
+    data in the JSON format
+"""
+
+import json
+import requests
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        sys.exit[1]
+
+    base_url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(base_url + "users/{}".format(sys.argv[1])).json()
+    username = user.get("username")
+    todos = requests.get(
+            base_url + "todos", params={
+                "userId": sys.argv[1]}).json()
+
+    with open("{}.json".format(sys.argv[1]), "w") as jsonfile:
+        json.dump({sys.argv[1]: [{
+            "task": t.get("title"),
+            "completed": t.get("completed"),
+            "username": username
+            } for t in todos]}, jsonfile)
